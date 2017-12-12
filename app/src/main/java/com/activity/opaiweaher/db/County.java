105ee731d96eeb3e14a647cdd2f02cf25aae5819
Leao <yccddd@163.com>
2017-12-12 19:14:20 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/activity/opaiweaher/db/County.java
@@ -0,0 +1,44 @@
+package com.activity.opaiweaher.db;
+
+/**
+ * Created by msi-pc on 2017/12/12.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/activity/opaiweaher/db/Province.java
@@ -0,0 +1,37 @@
+package com.activity.opaiweaher.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by msi-pc on 2017/12/12.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
